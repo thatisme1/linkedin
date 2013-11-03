da122ab5a4143dc2f529b60720d7fd075580eef2
thatisme1 <thatisme1@gmail.com>
2013-11-03 17:11:19 +0500

    abc

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Bootstrap::Application.config.secret_token = ENV['SECRET_TOKEN']
+#'a0e0588d6ea0c7e1e958f10b45dd2be195fda907e7fc20c34fc5b1ce88c86dc0c0f736f1a3b3e1adf04f0a61890b98589ed5f6061979e746a083663e6d573d05'
