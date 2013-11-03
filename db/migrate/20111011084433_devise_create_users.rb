517854cf866ef0ff38b92c084288a599e96412c3
thatisme1 <thatisme1@gmail.com>
2013-11-03 18:29:46 +0500

    abc

--- db/migrate/20111011084433_devise_create_users.rb
+++ db/migrate/20111011084433_devise_create_users.rb
@@ -1,11 +1,20 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
-      t.confirmable
+       t.string :email,              :null => false, :default => ""
+  t.string :encrypted_password, :null => false, :default => ""
+
+  ## Recoverable
+  t.string   :reset_password_token
+  t.datetime :reset_password_sent_at
+
+#      t.rememberable
+  t.integer  :sign_in_count, :default => 0
+  t.datetime :current_sign_in_at
+  t.datetime :last_sign_in_at
+  t.string   :current_sign_in_ip
+  t.string   :last_sign_in_ip
+      # t.confirmable
       # t.encryptable
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
